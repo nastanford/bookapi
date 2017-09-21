7f50b1d50f3edc681791d8becabebfb61db6b153
Nathan Stanford Sr <Nathan.Stanford@gmail.com>
2017-09-21 09:55:45 -0400

    Updated to fix the sent message to the browser.  Currently it is a hard coded message.

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var app = express();
 // Looks for port in the enviroment defaults to 3000 if not found.
 var port = process.env.PORT || 3000;
 
-app.get('/',function(req, res) {
-  req.setEncoding('Welcome to my API');
+
+app.get('/',function(req, res){
+  res.send('Welcome to my API');
 });
 
-app.listen(port, funciton() {
+app.listen(port, function(){
   console.log('Running on PORT: ' + port);
 });
